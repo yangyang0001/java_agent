782badc2646266bd6ab1515c3c3243bc13001131
Jianwei.Yang <jianwei.yang@myk-pq.net>
2026-07-05 15:59:13 +0900

    Hello Agents 学习第一章

---
+++ src/main/java/deepblue/AgentApplication.java
@@ -0,0 +1,13 @@
+package deepblue;
+
+/**
+ * Hello world!
+ *
+ */
+public class AgentApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/deepblue/inaction_01_hello_agents/chater_01/ObjectTest.java
@@ -0,0 +1,23 @@
+package deepblue.inaction_01_hello_agents.chater_01;
+
+/**
+ * 模拟多个参数的方式进行
+ */
+public class ObjectTest {
+
+    public static void main(String[] args) {
+
+        String param1 = "zhangsan";
+
+        Object result = process(param1, "aaaa", "bbbb");
+
+        System.out.println(result);
+
+    }
+
+    public static Object process(Object param1, Object ... params) {
+        // 模拟程序执行过程
+        return "process";
+    }
+
+}
